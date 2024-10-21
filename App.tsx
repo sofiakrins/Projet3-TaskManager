9425b524480d3d08b6e34aa4ae5545760b5255fd
sofiakrins <sofiaisabelkrins@gmail.com>
2024-10-21 18:18:10 -0400

    Test

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Login from '../REACT NATIVE/appli_native/app-native/components/Login';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome to the App</Text>
+      <Login />
+    </View>
+  );
+};
+
+export default App;
